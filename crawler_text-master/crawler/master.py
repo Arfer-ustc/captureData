86bee0a5770d79315e5f3bda6c938aaf04a00fd4
xuefei <1403193782@qq.com>
2018-11-03 21:37:50 +0800

    update

--- crawler_text-master/crawler/master.py
+++ crawler_text-master/crawler/master.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from urllib import request
+from lxml import etree
+from crawler.tasklist import TaskList
+import threading
 import json
 import math
 import socket
@@ -11,96 +14,116 @@ __ITEMS_PER_PAGE__ = 50
 
 
 class Master:
-    __tasks_waiting__ = None
-    __tasks_pending__ = None
+    __task_list__ = None
     __socket__ = None
 
     # 类初始化
     def __init__(self, addr, port):
-        self.__tasks_waiting__ = list()
-        self.__tasks_pending__ = list()
-        self.__tasks_period__ = list()
+        self.__task_list__ = TaskList(30)
         self.__socket__ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.__socket__.bind((addr, int(port)))
         self.__socket__.listen(__MAX_SLAVES__)
 
     # 获取文章URL列表 #
     def analyze_urls(self, entries):
+        print("Initializing tasks list...")
         for entry in entries:
             if "api_json" in entry:
-                self.__tasks_waiting__.extend(self.__parse_api_json__(entry, 150))
-            elif "url" in entry:
-                # TODO: use selenium to parse simple html
-                pass
+                t = threading.Thread(target=self.__parse_urls_from_json__, args=(entry, 150, self.__task_list__))
+                t.start()
+                # self.__parse_urls_from_json__(entry, 150, self.__task_list__)
+            elif "list_url" in entry:
+                t = threading.Thread(target=self.__parse_urls_from_page__, args=(entry, 150, self.__task_list__))
+                t.start()
+                # self.__parse_urls_from_page__(entry, 150, self.__task_list__)
+        time.sleep(2)
 
     # 监听网络端口以分发任务 #
     def dispatch(self):
         print("Waiting for slaves...")
         while True:
             # 若任务列表为空，则程序退出
-            if self.__is_tasks_list_empty():
-                print("All tasks done!")
-                break
+            if self.__task_list__.is_empty():
+                print("No task in list")
             # 接受来自Slave节点的连接
             conn, addr = self.__socket__.accept()
             try:
                 conn.settimeout(10)
                 # 接收请求数据
-                msg = json.loads(conn.recv(1024).decode("utf-8"))
+                req = json.loads(conn.recv(1024).decode("utf-8"))
                 # 若Slave的请求命令为get，则向其发送新任务并将任务加入pending队列中
-                if msg["cmd"] == "get":
+                if req["cmd"] == "get":
                     res = dict(
-                        status="ok",
+                        status=dict(
+                            code=0,
+                            msg="success"
+                        ),
                         data=dict(
                             news_url=self.__dispatch_task__()    # 调用dispatch_task()函数时会自动将任务加入pending队列
                         )
                     )
                     conn.send(json.dumps(res).encode("utf-8"))
-                    print("Dispatch {0} to slave {1}".format(res["data"], msg["id"]))
-                    print("{0} urls in pool".format(len(self.__tasks_waiting__)))
+                    print("Dispatch {0} to slave {1}".format(res["data"], req["id"]))
                 # 若Slave的请求命令为done，则将pending队列中相应的任务移除
-                elif msg["cmd"] == "done":
-                    self.__done_task__(msg["data"])
+                elif req["cmd"] == "done":
+                    self.__done_task__(req["data"]["news_url"])
                     res = dict(
-                        status="ok"
+                        status=dict(
+                            code=0,
+                            msg="success"
+                        ),
+                        data=""
                     )
                     conn.send(json.dumps(res).encode("utf-8"))
-                    print("Slave {0} done fetching '{1}'".format(msg["id"], msg["data"]))
+                    print("Slave {0} done fetching '{1}'".format(req["id"], req["data"]["news_url"]))
             except socket.timeout:
                 print("Connection timeout")
             conn.close()
 
-    # 从API中获取文章URL列表 #
+    # 从API中获取文章URL列表并存入tasklist中 #
     @staticmethod
-    def __parse_api_json__(entry, count):
+    def __parse_urls_from_json__(entry, count, task_list):
         max_page = math.ceil(count / __ITEMS_PER_PAGE__)
         url_pattern = entry["api_json"]
-        news_urls = list()
         for i in range(1, max_page + 1):
+            news_urls = list()
             url = url_pattern.format(__ITEMS_PER_PAGE__, i)
             res = request.urlopen(url, timeout=3)
             data = json.loads(res.read(), encoding='utf8')
             for news_item in data["result"]["data"]:
                 news_urls.append(news_item["url"])
-            time.sleep(2)
-        news_urls = list(set(news_urls))
-        return news_urls
+            task_list.put_tasks(news_urls)
+            time.sleep(1)
+
+    @staticmethod
+    def __parse_urls_from_page__(entry, count, task_list):
+        chrome_options = webdriver.ChromeOptions()  # 获取ChromeWebdriver配置文件
+        prefs = {"profile.managed_default_content_settings.images": 2}  # 设置不加载图片以加快速度
+        chrome_options.add_experimental_option("prefs", prefs)
+        chrome_options.add_argument("--headless")  # 不使用GUI界面
+        chrome_options.add_argument("--disable-gpu")  # 禁用GPU渲染加速
+        driver = webdriver.Chrome(chrome_options=chrome_options)  # 创建ChromeWebdriver
+        driver.set_page_load_timeout(10)  # 设置连接超时时间为15s
+        driver.get(entry["list_url"])
+
+        url_cnt = 0
+        while url_cnt < count:
+            if url_cnt != 0:
+                time.sleep(1)
+                driver.find_element_by_xpath(entry["xpaths"]["next"]).click()
+            selector = etree.HTML(driver.page_source)
+            urls = selector.xpath(entry["xpaths"]["url"])
+            url_cnt += len(urls)
+            task_list.put_tasks(urls)
+        driver.close()
 
     # 分发任务 #
     def __dispatch_task__(self):
-        if len(self.__tasks_waiting__) == 0:
+        news_url = self.__task_list__.get_task()
+        if news_url is None:
             return -1
-        news_url = self.__tasks_waiting__.pop()
-        self.__tasks_pending__.append(news_url)
         return news_url
 
     # 完成任务 #
     def __done_task__(self, news_url):
-        self.__tasks_pending__.remove(news_url)    # 将任务从pending列表中移除
-
-    # 检查任务列表是否已清空 #
-    def __is_tasks_list_empty(self):
-        if len(self.__tasks_waiting__) + len(self.__tasks_pending__) == 0:
-            return True
-        return False
-
+        self.__task_list__.done_task(news_url)    # 将任务从pending列表中移除
